1317e275167c166b1a8c93e96dfbb44bcca32ab7
Felix Kratzer <fez@Felixs-MBP.fritz.box>
2020-03-25 12:37:48 +0100

    More automation

--- update.sh
+++ update.sh
@@ -2,5 +2,10 @@
 # https://github.com/CSSEGISandData/COVID-19
 
 git pull --no-edit upstream master
+
 python3 basic_numbers.py
 python3 rates.py
+
+git add **/*.png
+git commit -m "Updated data"
+git push
