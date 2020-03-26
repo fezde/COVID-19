98f5dc8b78af1056d28e12df277e9afe52209f75
Felix Kratzer <fez@Felixs-MBP.fritz.box>
2020-03-26 18:06:57 +0100

    webpage new gets built and deployed correctly

--- update.sh
+++ update.sh
@@ -1,11 +1,16 @@
 # update data from the original data source
-# https://github.com/CSSEGISandData/COVID-19
+https://github.com/CSSEGISandData/COVID-19
 
 git pull --no-edit upstream master
 
 python3 basic_numbers.py
 python3 rates.py
 
-git add **/*.png
+cd gatsby
+npm run deploy
+cd ..
+
+
+git add charts/**/*.png
 git commit -m "Updated data"
 git push
