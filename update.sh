a981e20b549be95fe3f71b9be331dfb0b1ee3a03
Felix Kratzer <fez@Felixs-MBP.fritz.box>
2020-04-18 10:04:45 +0200

    Getting data using curl (instead of a merge)

--- update.sh
+++ update.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
+DOWN_BASE_URL=https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/
+DATA_DIR=csse_covid_19_data/csse_covid_19_time_series/
+
 
 # Calc a md5 of the relevant data files 
 function md5s()
 {
-    M1=`md5 -q csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv`
-    M2=`md5 -q csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv`
-    M3=`md5 -q csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv`
+    M1=`md5 -q "${DATA_DIR}time_series_covid19_confirmed_global.csv"`
+    M2=`md5 -q "${DATA_DIR}time_series_covid19_deaths_global.csv"`
+    M3=`md5 -q "${DATA_DIR}time_series_covid19_recovered_global.csv"`
     echo "$M1 $M2 $M3"
 }
 
@@ -15,9 +18,13 @@ MD_BEFORE=$(md5s)
 
 # Update data from the original data source
 # https://github.com/CSSEGISandData/COVID-19
-git pull --no-edit upstream master
-git merge --strategy-option ours -F README.md
-git add README.md
+
+DOWN_FILE=time_series_covid19_confirmed_global.csv
+curl -s "${DOWN_BASE_URL}${DOWN_FILE}" --output "${DATA_DIR}${DOWN_FILE}"
+DOWN_FILE=time_series_covid19_deaths_global.csv
+curl -s "${DOWN_BASE_URL}${DOWN_FILE}" --output "${DATA_DIR}${DOWN_FILE}"
+DOWN_FILE=time_series_covid19_recovered_global.csv
+curl -s "${DOWN_BASE_URL}${DOWN_FILE}" --output "${DATA_DIR}${DOWN_FILE}"
 
 # Get checksums after the git pull
 MD_AFTER=$(md5s)
@@ -27,6 +34,9 @@ if [ "$MD_BEFORE" == "$MD_AFTER" ]; then
         exit 0
     fi
 fi
+
+git add "${DATA_DIR}/*"
+git commit -m "Updated data from original data repository"
 
 make all
 
