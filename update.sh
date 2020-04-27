c7814a03f387c38156522822bcb470137a0fb830
fezde <fezde@users.noreply.github.com>
2020-04-27 08:40:43 +0200

    Revert last commit

--- update.sh
+++ update.sh
@@ -34,34 +34,34 @@ MD_BEFORE=$(md5s)
 # Update data from the original data source
 # https://github.com/CSSEGISandData/COVID-19
 
-#DOWN_FILE=time_series_covid19_confirmed_global.csv
-#curl -s "${DOWN_BASE_URL}${DOWN_FILE}" --output "${DATA_DIR}${DOWN_FILE}"
-#DOWN_FILE=time_series_covid19_deaths_global.csv
-#curl -s "${DOWN_BASE_URL}${DOWN_FILE}" --output "${DATA_DIR}${DOWN_FILE}"
-#DOWN_FILE=time_series_covid19_recovered_global.csv
-#curl -s "${DOWN_BASE_URL}${DOWN_FILE}" --output "${DATA_DIR}${DOWN_FILE}"
+DOWN_FILE=time_series_covid19_confirmed_global.csv
+curl -s "${DOWN_BASE_URL}${DOWN_FILE}" --output "${DATA_DIR}${DOWN_FILE}"
+DOWN_FILE=time_series_covid19_deaths_global.csv
+curl -s "${DOWN_BASE_URL}${DOWN_FILE}" --output "${DATA_DIR}${DOWN_FILE}"
+DOWN_FILE=time_series_covid19_recovered_global.csv
+curl -s "${DOWN_BASE_URL}${DOWN_FILE}" --output "${DATA_DIR}${DOWN_FILE}"
 
 # Get checksums after the git pull
 MD_AFTER=$(md5s)
-#if [ "$MD_BEFORE" == "$MD_AFTER" ]; then
-#    if [ "$1" != "--force" ]; then
-#        echo "Files have not chaged. Nothing to do here"
-#        exit 0
-#    fi
-#fi
+if [ "$MD_BEFORE" == "$MD_AFTER" ]; then
+    if [ "$1" != "--force" ]; then
+        echo "Files have not chaged. Nothing to do here"
+        exit 0
+    fi
+fi
 
-#git add "${DATA_DIR}/*"
-#git commit -m "Updated data from original data repository"
+git add "${DATA_DIR}/*"
+git commit -m "Updated data from original data repository"
 
 make all
 
-#DATE=`date "+%Y-%m-%d-%H"`
-#TAG="data-update_$DATE"
-#git tag "$TAG"
-#git push --tags
+DATE=`date "+%Y-%m-%d-%H"`
+TAG="data-update_$DATE"
+git tag "$TAG"
+git push --tags
 
 # Bring new charts to git
-#git add charts/**/*.png
-#git add charts/**/*.gif
-#git commit -m "Updated data"
-#git push
+git add charts/**/*.png
+git add charts/**/*.gif
+git commit -m "Updated data"
+git push
